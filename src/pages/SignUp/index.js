1c4403f5e8f4cc2a66112f605b8b75252cebe561
murilokrugner <murilokrugner@hotmail.com>
2019-08-31 16:19:07 -0300

    icons

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
+
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import { Container, Form, FormInput, SubmitButton, SignLink, SignLinkText } from './styles';
 
 export default function SignUp() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} style={{width: 200, height: 120}}/>
+
+        <Form>
+        <FormInput
+            icon="mail-outline"
+            keyboardType="default"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu nome"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>Cadastrar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Já tenho uma conta</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
